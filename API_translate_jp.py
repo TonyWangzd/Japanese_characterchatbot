44002371c51a37fb94a6fb56aa74b601e2744f88
017-010m <wang_zhaodong@ei.sanken.osaka-u.ac.jp>
2019-04-26 16:51:52 +0900

    add function to translate all the text

--- API_translate_jp.py
+++ API_translate_jp.py
@@ -1,11 +1,18 @@
 
-
 import requests
-import  re
-import urllib.parse
 import hashlib
 import random
 import json
+import pandas as pd
+import re
+import logging
+
+
+output_question_file = '/Users/017-010m/code/Japanese_characterchatbot/data/WikiQA-Question.tsv'
+output_answer_file = '/Users/017-010m/code/Japanese_characterchatbot/data/WikiQA-Answer.tsv'
+
+jp_question_file = '/Users/017-010m/code/Japanese_characterchatbot/data/JP-Question.tsv'
+jp_answer_file = '/Users/017-010m/code/Japanese_characterchatbot/data/JP-Answer.tsv'
 
 en_sentence = 'I am back.'
 key = '99sRkrxL4TEdQfU230OO'
@@ -45,5 +52,18 @@ def translate(sentence):
         return translate_text
 
 
+def translate_by_line(file_name, output_file):
+    #regex = re.compile(r'/d.+,?')
+    file1 = pd.read_csv(file_name, encoding='utf-8', header=None, sep=None)
+    for line in file1.iterrows():
+        line_text = line[1][1]
+        print(line_text)
+        jp_line_text = translate(line_text)
+        with open(output_file, 'a') as write_file:
+            write_file.write(jp_line_text+'\n')
+            print(jp_line_text, '\n', line_text)
+
+
 if __name__ == "__main__":
-    translate(en_sentence)
+    #translate_by_line(output_question_file,jp_question_file)
+    translate_by_line(output_answer_file,jp_answer_file)
