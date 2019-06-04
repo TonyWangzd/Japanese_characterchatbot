b4ee9ec5c2a65644e181681055a781da1dca907f
017-010m <wang_zhaodong@ei.sanken.osaka-u.ac.jp>
2019-06-04 21:13:01 +0900

    add short text for test

--- LSTM_mxnet.py
+++ LSTM_mxnet.py
@@ -3,22 +3,24 @@ import math
 from mxnet import autograd, nd
 from mxnet.gluon import loss as gloss
 import time
-from sklearn.externals import joblib
 import numpy as np
 import os
-
+from sklearn.externals import joblib
+import matplotlib.pyplot as plt
+import random
+import d2lzh as d2l
 
 file = './data/obama2.txt'
 
-num_steps = 10
+num_steps = 30
 
-num_epochs = 1000
+num_epochs = 3000
 
 clipping_theta = 0.01
 
 lr = 100
 
-batch_size = 10
+batch_size = 30
 
 pred_period = 1
 
@@ -26,12 +28,12 @@ pred_len = 30
 
 para_file = 'params_rnn.params'
 
+adagrad_state_feature = []
 
 
 def process_corpus(corpus_chars):
     corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
     corpus_words = nltk.word_tokenize(corpus_chars)
-
     index_to_word = list(set(corpus_words))
     word_to_index = dict([(word, i) for i, word in enumerate(index_to_word)])
 
@@ -43,12 +45,20 @@ def process_corpus(corpus_chars):
     print('sample:\n', sample)
     return index_to_word, word_to_index, corpus_indices, vocabulary_size
 
-def sample(preds, temprature):
-    preds = np.asarray(preds).astype('float64')
+
+def sample(preds_nd, temprature):
+    preds = preds_nd.asnumpy()
+    preds[preds < 0] = 0
+    preds = preds / np.sum(preds)
     preds = np.log(preds) / temprature
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
+    preds = preds.transpose()
+    preds = np.squeeze(preds)
+    #print('sum is' + str(sum(preds[:-1])))
+    preds = preds / (np.sum(preds)+0.02)
+    #print('sum is' + str(sum(preds[:-1])))
+    probas = np.random.multinomial(100, preds, 1)
     return np.argmax(probas)
 
 # 相邻采样
@@ -64,8 +74,19 @@ def data_iter_consecutive(corpus_indices, batch_size, num_steps, ctx=None):
         data = indices[:, i:i + num_steps]
         label = indices[:, i + 1:i + num_steps + 1]
         yield data, label
-# 随机采样
-#def data_iter_random(corpus_indices, batch_size, numsteps, ctx=None):
+
+
+def adagrad(params, states, lr):
+    eps = 1e-6
+    for p, s in zip(params, states):
+        s[:] += p.grad.square()
+        p[:] -= lr * p.grad / (s + eps).sqrt()
+
+
+def init_adagrad_states(vocab_size, num_hiddens):
+    s_w = nd.random.normal(scale=0.01, shape=[vocab_size, num_hiddens])
+    s_b = nd.random.normal(scale=0.01, shape=[num_hiddens, num_hiddens])
+    return s_w, s_b
 
 
 def rnn(inputs, state, params):
@@ -117,8 +138,53 @@ def predict_rnn(prefix, num_chars, rnn, params,
         if t < len(prefix) - 1:
             output.append(char_to_idx[prefix[t+1]])
         else:
-            output.append(int(Y[0].argmax(axis=1).asscalar()))
+            a = Y[0]
+            output_number = sample(a, 1.2)
+            output.append(output_number)
     return ' '.join([idx_to_char[i] for i in output])
+
+
+##########predict with check for practice
+def word2gram(corpus_words):
+    new_dic = []
+    for number in range(len(corpus_words)-1):
+        new_dic.append(corpus_words[number]+' '+corpus_words[number+1])
+    return new_dic
+
+
+def generate_with_check(corpus_words, num_chars, char_to_idx, idx_to_char, vocab_size):
+    word2gram_dic = word2gram(corpus_words)
+    text = input('please input the word you would like for seed----\n')
+    if len(text)>0:
+        prefix = nltk.word_tokenize(text)
+        state = init_rnn_state(1, num_hiddens, ctx)
+        output = [char_to_idx[prefix[0]]]
+
+        for t in range(num_chars + len(prefix) - 1):
+            X = to_onehot(nd.array([output[-1]], ctx=ctx), vocab_size)
+            (Y, state) = rnn(X, state, params)
+
+            if t < len(prefix) - 1:
+                output.append(char_to_idx[prefix[t + 1]])
+            else:
+                not_ingram = True
+                while not_ingram:
+                    i = 0
+                    output_number = sample(Y[0], 0.1)
+                    check_gram = idx_to_char[output[-1]]+' '+idx_to_char[output_number]
+
+                    for element in word2gram_dic:
+                        if element == check_gram:
+                            not_ingram = False
+                            break
+
+                    if not not_ingram or i > 5:
+                        output.append(output_number)
+                    else:
+                        i += 1
+
+        return ' '.join([idx_to_char[i] for i in output])
+
 
 def grad_clipping(params, theta):
     norm = nd.array([0])
@@ -129,9 +195,11 @@ def grad_clipping(params, theta):
         for param in params:
             param.grad[:] *= theta / norm
 
+
 def sgd(params, lr, batch_size):
     for param in params:
         param[:] = param - lr * param.grad / batch_size
+
 
 def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
                           vocab_size, ctx, corpus_indices, idx_to_char,
@@ -152,13 +220,15 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
     loss = gloss.SoftmaxCrossEntropyLoss()
 
     for epoch in range(num_epochs):
-        if (epoch % 10) == 0:
-            lr = 0.8*lr
+        if epoch > 0:
+            if epoch % 20 == 0:
+                lr = 0.85 * lr
         print('training...now is on epoch'+str(epoch)+'\n')
-        #在epoch开始时初始化隐藏状态
+        # 在epoch开始时初始化隐藏状态
         state = init_rnn_state(batch_size, num_hiddens, ctx)
         l_sum, n, start = 0.0, 0, time.time()
         data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, ctx)
+
         for X, Y in data_iter:
             for s in state:
                 s.detach()
@@ -175,7 +245,7 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
                 l = loss(outputs, y).mean()
             l.backward()
             grad_clipping(params, clipping_theta)  # 裁剪梯度
-            sgd(params, lr, 1)  # 因为误差已经取过均值，梯度不用再做平均
+            sgd(params, lr, 1)
             l_sum += l.asscalar() * y.size
             n += y.size
         #print loss
@@ -184,6 +254,7 @@ def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
         if (epoch + 1) % pred_period == 0:
             print('epoch %d, perplexity %f, time %.2f sec' % (
                 epoch + 1, math.exp(l_sum / n), time.time() - start))
+            print('\nlearning rate now is'+str(lr))
             for prefix in prefixes:
                 print(' -',
                     predict_rnn(
@@ -199,6 +270,12 @@ if __name__ == "__main__":
     text = open(file, encoding='utf=8').read()
 
     index_to_word, word_to_index, corpus_indices, vocabulary_size = process_corpus(text)
+
+    corpus_bind = [index_to_word, word_to_index, corpus_indices, vocabulary_size]
+
+    joblib.dump(corpus_bind, 'corpus_bind.pkl')
+
+    print('successfully save the language model\n')
 
     num_inputs, num_hiddens, num_outputs = vocabulary_size, 256, vocabulary_size
     # parameter for three layers
